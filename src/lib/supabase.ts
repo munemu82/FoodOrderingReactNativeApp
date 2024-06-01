1fbf42eeb7455edc76b5a5beab80681aca3bab65
munemu82 <munemu82@gmail.com>
2024-06-02 00:21:48 +1000

    Basic authentication

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://skisbwtyxeyeqvjpsnui.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNraXNid3R5eGV5ZXF2anBzbnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNTc2NDYsImV4cCI6MjAzMjYzMzY0Nn0.RC0-DHBSFGShwqVciHj2M0Z-HBgPgI7pA1aFOctAcLE';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
